d564032d6bbe8384e3e55c32961f4cf25198f41c
saiem-mohit <72776550+saiem-mohit@users.noreply.github.com>
2020-12-20 12:02:03 +0600

    2nd commit

---
+++ FINAL LAB XM/userservice.php
@@ -0,0 +1,69 @@
+<?php
+	require_once('db.php');
+
+	function getByID($id){
+		$conn = dbConnection();
+
+		if(!$conn){
+			echo "DB connection error";
+		}
+
+		$sql = "select * from users where id={$id}";
+		$result = mysqli_query($conn, $sql);
+		$row = mysqli_fetch_assoc($result);
+		return $row;
+	}
+
+	function getAllUser(){
+		$conn = dbConnection();
+
+		if(!$conn){
+			echo "DB connection error";
+		}
+
+		$sql = "select * from users";
+		$result = mysqli_query($conn, $sql);
+		$users = [];
+
+		while($row = mysqli_fetch_assoc($result)){
+			array_push($users, $row);
+		}
+
+		return $users;
+	}
+
+
+	function validate($user){
+		$conn = dbConnection();
+
+		if(!$conn){
+			echo "DB connection error";
+		}
+
+		$sql = "select * from users where username='{$user['username']}' and password='{$user['password']}'";
+		$result = mysqli_query($conn, $sql);
+		$user = mysqli_fetch_assoc($result);
+
+		if(count($user) > 0 ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
+	function update($user){
+		$conn = dbConnection();
+		if(!$conn){
+			echo "DB connection error";
+		}
+
+		$sql = "update users set username='{$user['username']}', password='{$user['password']}', where id={$user['id']}";
+
+		if(mysqli_query($conn, $sql)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+?> 
